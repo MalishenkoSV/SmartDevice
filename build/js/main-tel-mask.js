3191acb3be8af129a68e562eee2972fa6659d7e7
Svetlana <lisalana15@gmail.com>
2020-04-23 09:11:28 +0300

    Исправляет часть багов

--- build/js/main-tel-mask.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var telInputs = document.querySelectorAll('input[type="tel"]');
-
-if (telInputs) {
-  telInputs.forEach(function (input) {
-    Inputmask('+7 (999) 999-99-99').mask(input);
-  });
-}
